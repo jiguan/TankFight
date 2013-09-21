73dd0353dc21327b01f2f15908af64e04190c5d9
jiguan <gjqvictory888@gmail.com>
2013-09-21 14:02:29 -0400

    delete Restaurant

--- RestaurantManagement/src/Tools.java
+++
@@ -1,7 +0,0 @@
-import java.awt.Font;
-
-public class Tools {
-	public static Font font_plain = new Font("Arial", Font.PLAIN, 16);
-	public static Font font_plain_small = new Font("Arial", Font.PLAIN,12);
-	public static Font font_blod_14 = new Font("Arial", Font.BOLD,14);
-}
